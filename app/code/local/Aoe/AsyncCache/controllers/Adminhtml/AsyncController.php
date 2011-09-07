22bed1fbffd06dbef42fe2d36052ce8c6131a129
Fabrizio Branca <github@fabrizio-branca.de>
2011-09-07 10:35:09 +0200

    Added check

--- app/code/local/Aoe/AsyncCache/controllers/Adminhtml/AsyncController.php
+++ app/code/local/Aoe/AsyncCache/controllers/Adminhtml/AsyncController.php
@@ -2,31 +2,33 @@
 
 /**
  * Async controller
- * 
+ *
  * @author Fabrizio Branca
  */
 class Aoe_AsyncCache_Adminhtml_AsyncController extends Mage_Adminhtml_Controller_Action {
-	
+
 	/**
 	 * Process the queue
 	 * This action is called from a button in the "Cache Management" module.
 	 * Afterwards it redirects back to that module
-	 * 
+	 *
 	 * @return void
 	 */
 	public function processAction() {
 		$processedJobs = Mage::getModel('aoeasynccache/cleaner')->processQueue();
-		foreach ($processedJobs as $job) {
-			$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__('Cleared cache with mode "%s" using tags "%s" (Duration: %s sec)', $job['mode'], implode(', ',$job['tags']), $job['duration']));
+		if (is_array($processedJobs)) {
+			foreach ($processedJobs as $job) {
+				$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__('Cleared cache with mode "%s" using tags "%s" (Duration: %s sec)', $job['mode'], implode(', ',$job['tags']), $job['duration']));
+			}
 		}
 		$this->_redirect('*/cache/index');
 	}
-	
+
 	/**
 	 * Delete a async entry
 	 * This action is called from a button in the "Cache Management" module.
 	 * Afterwards it redirects back to that module
-	 * 
+	 *
 	 * @return void
 	 */
 	public function deleteAction() {
@@ -38,6 +40,6 @@ class Aoe_AsyncCache_Adminhtml_AsyncController extends Mage_Adminhtml_Controller
 		$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__('Deleted item "%s"', $id));
 		$this->_redirect('*/cache/index');
 	}
-	
+
 }
 
