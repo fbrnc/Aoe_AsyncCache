f9d1330e7c8c5a545c40ccc66ecffd7f1f7474eb
Fabrizio Branca <mail@fabrizio-branca.de>
2011-12-01 20:43:37 +0100

    Added Flush Cache Store Now

--- app/code/local/Aoe/AsyncCache/controllers/Adminhtml/AsyncController.php
+++ app/code/local/Aoe/AsyncCache/controllers/Adminhtml/AsyncController.php
@@ -29,6 +29,32 @@ class Aoe_AsyncCache_Adminhtml_AsyncController extends Mage_Adminhtml_Controller
 	}
 
 	/**
+	 * Process the queue
+	 * This action is called from a button in the "Cache Management" module.
+	 * Afterwards it redirects back to that module
+	 *
+	 * @return void
+	 */
+	public function flushAllNowAction() {
+
+		Mage::dispatchEvent('adminhtml_cache_flush_all');
+		Mage::app()->getCacheInstance()->flush();
+		$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__("The cache storage has been flushed."));
+
+		$processedJobs = Mage::getModel('aoeasynccache/cleaner')->processQueue();
+		if (is_array($processedJobs)) {
+			foreach ($processedJobs as $job) {
+				if (count($job['tags'])) {
+					$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__('Cleared cache with mode "%s" using tags "%s" (Duration: %s sec)', $job['mode'], implode(', ',$job['tags']), $job['duration']));
+				} else {
+					$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__('Cleared cache with mode "%s" (Duration: %s sec)', $job['mode'], $job['duration']));
+				}
+			}
+		}
+		$this->_redirect('*/cache/index');
+	}
+
+	/**
 	 * Delete a async entry
 	 * This action is called from a button in the "Cache Management" module.
 	 * Afterwards it redirects back to that module
