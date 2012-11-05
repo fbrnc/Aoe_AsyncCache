72ced3d7e752e531cf98603919626e294c293a8d
Fabrizio Branca <github@fabrizio-branca.de>
2012-11-05 09:24:45 -0800

    Disabling the flag, so that it will always log

--- app/code/local/Varien/Cache/Core.php
+++ app/code/local/Varien/Cache/Core.php
@@ -129,7 +129,7 @@ class Varien_Cache_Core extends Zend_Cache_Core
 				$asynccache->setMode($mode);
 				$asynccache->setTags(is_array($tags) ? implode(',', $tags) : $tags);
 				$asynccache->setStatus('pending');
-				if (Mage::getStoreConfig('system/aoeasynccache/enabletrace') == 1) {
+				if (true || Mage::getStoreConfig('system/aoeasynccache/enabletrace') == 1) {
 					$asynccache->setTrace(Mage::app()->getHelper('aoeasynccache')->debugTrail());
 				}
 				try {
