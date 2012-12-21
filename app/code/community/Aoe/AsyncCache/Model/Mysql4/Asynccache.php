23906860e34511d2c9f42fe6fad08a322d97a711
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2012-12-21 02:12:32 -0800

    Added @method annotations

--- app/code/community/Aoe/AsyncCache/Model/Mysql4/Asynccache.php
+++ app/code/community/Aoe/AsyncCache/Model/Mysql4/Asynccache.php
@@ -4,6 +4,21 @@
  * Asynccache
  * 
  * @author Fabrizio Branca
+ *
+ * @method getId()
+ * @method setId()
+ * @method getTstamp()
+ * @method setTstamp()
+ * @method getMode()
+ * @method setMode()
+ * @method getTags()
+ * @method setTags()
+ * @method getTrace()
+ * @method setTrace()
+ * @method getStatus()
+ * @method setStatus()
+ * @method getProcessed()
+ * @method setProcessed()
  */
 class Aoe_AsyncCache_Model_Mysql4_Asynccache extends Mage_Core_Model_Mysql4_Abstract {
 	
