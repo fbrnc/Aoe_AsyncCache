6e6bea376a67e529dfd1a3db713b557fd6cbd3e3
Daniel Zohm <daniel.zohm@aoemedia.de>
2013-02-05 19:41:07 +0100

    overwritten save method.

--- app/code/community/Aoe/AsyncCache/Model/Mysql4/Asynccache.php
+++ app/code/community/Aoe/AsyncCache/Model/Mysql4/Asynccache.php
@@ -16,4 +16,31 @@ class Aoe_AsyncCache_Model_Mysql4_Asynccache extends Mage_Core_Model_Mysql4_Abst
 		$this->_init('aoeasynccache/asynccache', 'id');
 	}
 
+    /**
+     * Overwritten save method, updates data on duplicate key
+     *
+     * @param Mage_Core_Model_Abstract $object
+     * @return Aoe_AsyncCache_Model_Mysql4_Asynccache
+     */
+    public function save(Mage_Core_Model_Abstract $object)
+    {
+        if ($object->isDeleted()) {
+            return $this->delete($object);
+        }
+
+        $this->_serializeFields($object);
+        $this->_beforeSave($object);
+
+        $this->_getWriteAdapter()->insertOnDuplicate(
+            $this->getMainTable(),
+            $this->_prepareDataForSave($object),
+            $this->_fieldsForUpdate
+        );
+
+        $this->unserializeFields($object);
+        $this->_afterSave($object);
+
+        return $this;
+    }
+
 }
